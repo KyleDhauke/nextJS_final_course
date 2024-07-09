2f2002033ee25cc8a7fe0c83eafeb2e23c74d5fd
Kyle Dhauke <kyledhauke@gmail.com>
2024-07-09 11:32:43 +0100

    fix(next005): imported useRouter in Search.tsx

--- next005/app/components/Search.tsx
+++ next005/app/components/Search.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import { useState } from "react"
-import {useRouter} from "next/navigation"
+import { useRouter } from "next/navigation"
 
 export default function search() {
+  const [search, setSearch] = useState('');
+  const router = useRouter()
   return (
     <div>search</div>
+
   )
 }
